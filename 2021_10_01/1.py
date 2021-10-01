ff6b8b830f51fd60c2f51db2fa1e70251ee4c620
chlendyd7 <88640035+chlendyd7@users.noreply.github.com>
2021-10-01 23:22:59 +0900

    Add files via upload

---
+++ 2021_10_01/1.py
@@ -0,0 +1,25 @@
+print("""가나다라마바사
+    아자차카타파하""")
+print("Realease the kraken!" "No, wait!")
+start= 'b'
+middle='Hey ' *3+'\n'
+end="Goodbye bye bye bye ba by"
+print(start+start+middle+end)
+
+print(len(end))
+
+print(end.split(start))
+crypto_list=['Yeti', 'Bigfoot', 'Loch ness Monster']
+crypto_string = 'a'.join(crypto_list)
+print('Found and singing book deals:',crypto_string)
+
+setup="a duck goes into a bar..."
+print(setup.replace('duck','hi'))
+print(setup)
+
+import string
+print(string.punctuation)
+
+blurt = "What the...!!?"
+print(blurt.strip(string.punctuation))
+
